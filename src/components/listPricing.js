0018b18321bb8c34d4bff57a3706f5b4fab6bfe4
Mads Røjkum <roejkum@gmail.com>
2019-04-03 22:04:01 +0200

    Updating sections and adding new ones

--- src/components/listPricing.js
+++ src/components/listPricing.js
@@ -1,17 +1,16 @@
 (function() {
-	const listPricing = document.getElementsByClassName("list-pricing");
+	const priceCaptions = document.getElementsByClassName("price-caption");
 
-	if (listPricing) {
-		for (let i = 0; i < listPricing.length; i++) {
+	if( priceCaptions.length > 0 ) {
 
-			const priceCaptions = listPricing[i].getElementsByClassName("price-caption");
+		if ( priceCaptions[0].offsetHeight > priceCaptions[1].offsetHeight) {
 
-			if ( priceCaptions[0].offsetHeight > priceCaptions[1].offsetHeight) {
-				priceCaptions[1].style.height = priceCaptions[0].offsetHeight + 'px';
+			priceCaptions[1].style.height = priceCaptions[0].offsetHeight + 'px';
 
-			} else if ( priceCaptions[0].offsetHeight < priceCaptions[1].offsetHeight ) {
-				priceCaptions[0].style.height = priceCaptions[1].offsetHeight + 'px';
-			}
+		} else if ( priceCaptions[0].offsetHeight < priceCaptions[1].offsetHeight ) {
+
+			priceCaptions[0].style.height = priceCaptions[1].offsetHeight + 'px';
+
 		}
 	}
 
